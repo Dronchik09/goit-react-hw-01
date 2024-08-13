f21a08bb26b41d204fd429af054ceaf62d66066b
Andriy_Chornobay <andriy.chornobay@gmail.com>
2024-08-13 17:02:33 +0300

    add initial files

--- src/components/friendlist/Friendlist.jsx
+++ src/components/friendlist/Friendlist.jsx
@@ -1,17 +1,12 @@
 import { listItem, list } from "./Friendlist.module.css";
-import css from "./Friendlist.module.css";
-import clsx from "clsx";
+import Friendlistitem from "./Friendlistitem";
 export default function FriendList({ friends }) {
   return (
     <ul className={list}>
-      {friends.map(( {avatar, name, isOnline, id}) => {
+      {friends.map(item => {
         return (
-          <li className={listItem} key={id}>
-            <img src={avatar} alt="Avatar" width="48" />
-            <p className={css.nameTitle}>{name}</p>
-            <p className={clsx(isOnline ? css.online : css.offline)}>
-              {isOnline ? "online" : "offline"}
-            </p>
+          <li className={listItem} key={item.id}>
+            <Friendlistitem item={item} />
           </li>
         );
       })}
